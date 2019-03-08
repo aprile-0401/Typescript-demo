6d4292617b2db8b108dfaf0c62b0594d081abfb4
XinWang <xin.wang@target-energysolutions.com>
2019-03-08 15:12:25 +0800

    init project

---
+++ src/index.tsx
@@ -0,0 +1,41 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import Hello from './components/Hello';
+//import './index.css';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(
+  <Hello />,
+  document.getElementById('root') as HTMLElement
+);
+registerServiceWorker();
+
+class Animal {
+  name: string;
+  constructor(theName: string) { this.name = theName; }
+  move(distanceInMeters: number = 0) {
+      console.log(`${this.name} moved ${distanceInMeters}m.`);
+  }
+}
+
+class Snake extends Animal {
+  constructor(name: string) { super(name); }
+  move(distanceInMeters = 5) {
+      console.log("Slithering...");
+      super.move(distanceInMeters);
+  }
+}
+
+class Horse extends Animal {
+  constructor(name: string) { super(name); }
+  move(distanceInMeters = 45) {
+      console.log("Galloping...");
+      super.move(distanceInMeters);
+  }
+}
+
+let sam = new Snake("Sammy the Python");
+let tom: Animal = new Horse("Tommy the Palomino");
+
+sam.move();
+tom.move(34);
